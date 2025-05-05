339d6f83cb2c1a91263793eaa802e8bf7622d866
yash8107 <yashsinghal8107@gmail.com>
2025-05-05 23:22:49 +0530

    updating code

--- ondc-crypto-sdk-nodejs/src/types.d.ts
+++
@@ -1,6 +0,0 @@
-// src/types.d.ts
-declare module '*/subscription-controller' {
-    import { SubscriptionController } from '../controllers/subscription-controller';
-    const controller: SubscriptionController;
-    export default controller;
-}
